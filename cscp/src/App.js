e522bcc50ca7675b6db6c11dd9ccce05c444a4df
Aswinkrishna <aswinkrishnatu@gmail.com>
2023-11-17 00:19:52 +0530

    Tailwind added

---
+++ cscp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h>Hello</h>
+      </header>
+    </div>
+  );
+}
+
+export default App;
