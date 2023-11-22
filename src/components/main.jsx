6c1e6c4e5d5e457c44d5c98d10efce76b46ba882
aharonmathews <aharonmathews04@gmail.com>
2023-11-22 22:58:31 +0530

    New vite

---
+++ src/components/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../index.css';
+import Main from './main';
+import Navbar from './navbar';
+import Team from './team';
+import Parallax from './parallax';
+import Contact from './ContactPage'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <Team />
+  </div>
+);
+
+
+
+
+
---
+++ src/components/parcomp/img2.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Parallax,Background} from 'react-parallax';
+import code from '../../assets/code1.jpg'
+
+
+
+function Img(){
+    return (
+        <Parallax strength={500}>
+            <Background className='image'>
+                <img className='image' src={code} alt='new' />
+            </Background>
+            <div className='content'>
+                <span className='text'>Experience the depth of Programming</span>
+            </div>
+        </Parallax>
+    )
+}
+
+export default Img;
